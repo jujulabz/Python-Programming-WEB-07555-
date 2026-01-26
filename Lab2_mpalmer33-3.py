57a7eaee669b41d4f5bf685f64956f7890bb7374
jujulabz <153671334+jujulabz@users.noreply.github.com>
2026-01-25 23:53:01 -0500

    Created string for  the reverse message and then i added a print message using f string that outputs the orginal message as well as the reversed message both message will be printed on sperate lines i made sure of that by adding the newline after the f string.

--- Lab2_mpalmer33-3.py
+++ Lab2_mpalmer33-3.py
@@ -9,3 +9,9 @@ Any info about starter code (If used, where it came from, link, etc.),
 print("Hello there! This program will take your input and print out your orginal message as well as a backwards version.")
 
 User= input("Enter a string message: ")
+
+reversed_string = User[::-1]
+
+print(f'Orginal message:{user}' f'\nreversed message:(reversed_string)')
+
+
