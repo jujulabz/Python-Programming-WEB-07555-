984129c3ddd08db0d7352d18c241d586a7b64a4c
jujulabz <153671334+jujulabz@users.noreply.github.com>
2026-01-25 23:39:48 -0500

    Adding my Docstring

---
+++ Lab2_mpalmer33-3.py
@@ -0,0 +1,8 @@
+"""
+String Manipulator
+Melissa Palmer,
+The purpose of this program is to take user input and manipulate it to print it out backwards at the end.,
+Any info about starter code (If used, where it came from, link, etc.),
+1/25/26
+ """
+ 
