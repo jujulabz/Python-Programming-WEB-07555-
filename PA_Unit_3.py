254adcc224684c0283aa1450a12c5ab87aaf7939
jujulabz <153671334+jujulabz@users.noreply.github.com>
2026-02-01 19:05:59 -0500

    3.8 PA _Unit3 code

---
+++ PA_Unit_3.py
@@ -0,0 +1,28 @@
+Locations = ["Asia","Peru","Brazil","Rome"] #List
+
+print(Locations) #Orginal List
+
+Locations.sort #
+
+print(sorted(Locations)) #Alphabitical Order
+
+print(Locations) # og list
+
+Locations.sort(reverse=True)
+
+print(Locations) # Reverse Alphabitical
+
+Locations.reverse ()
+
+print(Locations) #reverse 
+
+Locations.reverse ()
+
+print(Locations)
+
+Locations.sort
+
+print(Locations)
+
+Locations.sort(reverse=True)
+print(Locations)
