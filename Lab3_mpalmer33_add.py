db284ef466d9a89ba463c0ec8ba807e379596d30
jujulabz <153671334+jujulabz@users.noreply.github.com>
2026-02-01 21:39:04 -0500

    Appending new Locations

--- Lab3_mpalmer33_add.py
+++ Lab3_mpalmer33_add.py
@@ -1,2 +1,8 @@
 from Lab3_mpalmer33_list import Locations
 
+Locations.append("Ethopia")
+Locations.append("Germeny")
+Locations.append("Iran")
+Locations.append("El Salvador")
+Locations.append("Brazil")
+
