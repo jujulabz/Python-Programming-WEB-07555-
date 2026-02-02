4f3e2869b2ac116f03c464b3332951408f46a8ff
jujulabz <153671334+jujulabz@users.noreply.github.com>
2026-02-01 21:28:03 -0500

    Adding Documentation + added my List

---
+++ Lab3_mpalmer33_list.py
@@ -0,0 +1,12 @@
+"""
+List
+Melissa Palmer,
+The purpose of this program is to get used to creating python script files and working with lists.,
+Any info about starter code (If used, where it came from, link, etc.),
+1/25/26
+ """
+ 
+Locations = ["Cuba","Japan","Vietnam","China","Peru","Jordan","Californa","Rome","Grecce","Thailand"]
+
+
+
