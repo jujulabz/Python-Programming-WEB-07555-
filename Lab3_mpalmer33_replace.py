ef0101b25cce50593d7e43a0ea57c8e5351e9ab9
jujulabz <153671334+jujulabz@users.noreply.github.com>
2026-02-01 22:11:18 -0500

    Printing out my locations as well as slicing through my list

--- Lab3_mpalmer33_replace.py
+++ Lab3_mpalmer33_replace.py
@@ -2,3 +2,8 @@ from Lab3_mpalmer33_add import Locations
 
 Locations = [Location.replace("Japan","binoculars")for Location in Locations]
 
+print (Locations)
+
+print (Locations[0:5])
+
+print (Locations[5:15])
